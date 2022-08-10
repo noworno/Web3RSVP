795bba85ba206235b7c2c6b5c90a8d3cee1f43b3
noworno <jlzhangood@163.com>
2022-08-10 10:30:42 +0800

    section 4 completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,10 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
-      chainId: 4,
+      chainId: 1337,
     },
-    rinkeby: {
-      url: "${process.env.STAGING_INFURA_URL}",
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
       accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
       gas: 2100000,
       gasPrice: 8000000000,
